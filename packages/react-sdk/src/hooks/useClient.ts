51dd1cbf5c378217035891c41cfab64b45ddeef5
Ry Racherbaumer <ry@xmtp.com>
2023-04-04 19:34:29 -0500

    Update React SDK
    * Update package description
    * Update hook descriptions
    * Allow send message options when calling the hook and sending a message
    * Move `keys` option to `initialize` function of `useClient`

--- packages/react-sdk/src/hooks/useClient.ts
+++ packages/react-sdk/src/hooks/useClient.ts
@@ -1,4 +1,6 @@
 import { useContext, useEffect } from "react";
+import type { Signer } from "@xmtp/xmtp-js";
+import { Client } from "@xmtp/xmtp-js";
 import type { InitClientArgs } from "../contexts/XMTPContext";
 import { XMTPContext } from "../contexts/XMTPContext";
 
@@ -6,7 +8,10 @@ import { XMTPContext } from "../contexts/XMTPContext";
  * This hook allows you to initialize, disconnect, and access the XMTP client
  * instance. It also exposes the error and loading states of the client.
  */
-export const useClient = ({ keys, options, signer }: InitClientArgs) => {
+export const useClient = ({
+  options,
+  signer,
+}: Pick<InitClientArgs, "options" | "signer">) => {
   const xmtpContext = useContext(XMTPContext);
   if (xmtpContext === undefined) {
     console.error("useClient must be used within a XMTPProvider");
@@ -23,7 +28,8 @@ export const useClient = ({ keys, options, signer }: InitClientArgs) => {
     client: xmtpContext.client,
     disconnect: xmtpContext.closeClient,
     error: xmtpContext.error,
-    initialize: () => xmtpContext.initClient({ keys, options, signer }),
+    initialize: (keys?: InitClientArgs["keys"]) =>
+      xmtpContext.initClient({ keys, options, signer }),
     isLoading: xmtpContext.isLoading,
   };
 };
