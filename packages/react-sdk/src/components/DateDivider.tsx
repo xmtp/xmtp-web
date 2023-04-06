295550d7437875d43c80de9f34e35398c8c808b2
Ry Racherbaumer <ry@xmtp.com>
2023-04-06 09:51:58 -0500

    Remove Tailwind from DateDivider component

--- packages/react-sdk/src/components/DateDivider.tsx
+++ packages/react-sdk/src/components/DateDivider.tsx
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import styles from "./DateDivider.module.css";
 
 export type DateDividerProps = {
   /**
@@ -8,11 +9,7 @@ export type DateDividerProps = {
 };
 
 export const DateDivider: React.FC<DateDividerProps> = ({ date }) => (
-  <div className="flex align-items-center items-center pb-8 pt-4">
-    <div className="grow h-0.5 bg-gray-300/25" />
-    <span className="mx-2 flex-none text-gray-500 text-sm font-bold">
-      {format(date, "PPP")}
-    </span>
-    <div className="grow h-0.5 bg-gray-300/25" />
+  <div className={styles.wrapper}>
+    <div className={styles.date}>{format(date, "PPP")}</div>
   </div>
 );
