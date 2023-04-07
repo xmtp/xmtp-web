51cad02ac2cd66e7a31330d9126ca65d6e4d92bd
Ry Racherbaumer <ry@xmtp.com>
2023-04-07 13:14:42 -0500

    Remove Tailwind from SDK and example app

--- packages/react-sdk/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{ts,tsx,css}", "./.storybook/globals.css"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
-};
