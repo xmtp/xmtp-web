e761283fe25efd9f9955047a24936da5637fe89f
Ry Racherbaumer <ry@xmtp.com>
2023-12-18 14:12:48 -0600

    Upgrade to latest JS SDK beta

--- examples/react-quickstart/vite.config.ts
+++ examples/react-quickstart/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@xmtp/user-preferences-bindings-wasm"],
+  },
 });
