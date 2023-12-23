4e1a8845eacd32d42bdb2d4a2967ea7bf65a173e
fabri <fguespe@gmail.com>
2023-12-23 15:16:23 -0300

    fixes

--- packages/react-sdk/src/helpers/caching/contentTypes/CustomContentType.test.ts
+++ packages/react-sdk/src/helpers/caching/contentTypes/CustomContentType.test.ts
@@ -1,10 +1,6 @@
-import { it, expect, describe, vi, beforeEach } from "vitest";
-import { Client, DecodedMessage, ContentTypeId } from "@xmtp/xmtp-js";
-import { Wallet } from "ethers";
-import { saveMessage, getMessageByXmtpID } from "@/helpers/caching/messages";
+import { it, expect, describe, beforeEach } from "vitest";
+import { ContentTypeId } from "@xmtp/xmtp-js";
 import { getDbInstance, clearCache } from "@/helpers/caching/db";
-import type { CachedMessageWithId } from "@/helpers/caching/messages";
-import { createRandomWallet } from "@/helpers/testing";
 
 // Create a unique identifier for your content type
 const ContentTypeMultiplyNumbers = new ContentTypeId({
@@ -16,59 +12,47 @@ const ContentTypeMultiplyNumbers = new ContentTypeId({
 
 // Define the MultiplyCodec class
 class ContentTypeMultiplyNumberCodec {
+  // eslint-disable-next-line class-methods-use-this
   get contentType() {
     return ContentTypeMultiplyNumbers;
   }
 
   // The encode method accepts an object with two numbers (a, b) and encodes it as a byte array
-  encode({ a, b }: { a: number; b: number }) {
-    return {
-      type: ContentTypeMultiplyNumbers,
-      parameters: {},
-      content: new TextEncoder().encode(JSON.stringify({ a, b })),
-    };
-  }
+  // eslint-disable-next-line class-methods-use-this
+  encode = ({ a, b }: { a: number; b: number }) => ({
+    type: ContentTypeMultiplyNumbers,
+    parameters: {},
+    content: new TextEncoder().encode(JSON.stringify({ a, b })),
+  });
 
   // The decode method decodes the byte array, parses the string into numbers (a, b), and returns their product
-  decode(content: { content: any }) {
+  // eslint-disable-next-line class-methods-use-this
+  decode = (content: { content: Uint8Array }) => {
     const uint8Array = content.content;
-    const { a, b } = JSON.parse(new TextDecoder().decode(uint8Array));
+    const { a, b } = JSON.parse(new TextDecoder().decode(uint8Array)) as {
+      a: number;
+      b: number;
+    };
     return a * b;
-  }
+  };
 
-  fallback(content: number): string | undefined {
-    return `Can’t display number content types. Number was ${JSON.stringify(
-      content,
-    )}`;
-    // return undefined; if you don't want the content type to be displayed.
-  }
+  // eslint-disable-next-line class-methods-use-this
+  fallback = (content: number): string | undefined =>
+    `Can’t display number content types. Number was ${JSON.stringify(content)}`;
+  // return undefined; if you don't want the content type to be displayed.
 }
 
-export const multiplyNumbersContentTypeConfig: ContentTypeConfiguration = {
+export const multiplyNumbersContentTypeConfig = {
   codecs: [new ContentTypeMultiplyNumberCodec()],
   contentTypes: [ContentTypeMultiplyNumbers.toString()],
   namespace: "multiplyNumbers", // Replace with the actual namespace you are using
   processors: {
-    [ContentTypeMultiplyNumbers.toString()]: [processMultiplyNumbers],
+    [ContentTypeMultiplyNumbers.toString()]: [],
   },
   validators: {
-    [ContentTypeMultiplyNumbers.toString()]: isValidMultiplyNumbersContent,
+    [ContentTypeMultiplyNumbers.toString()]: [],
   },
 };
-
-// You'll need to define the processMultiplyNumbers and isValidMultiplyNumbersContent functions
-// These should be tailored to handle the specific logic of processing and validating
-// the content type for multiplying numbers.
-
-function processMultiplyNumbers(content) {
-  // Define how to process the multiply numbers content
-  // Example: logging, storing, or manipulating the data
-}
-
-function isValidMultiplyNumbersContent(content) {
-  // Define validation logic for multiply numbers content
-  // Example: checking if the numbers are valid, not null, etc.
-}
 
 const db = getDbInstance();
 
@@ -91,12 +75,12 @@ describe("ContentTypeMultiplyNumberCodec", () => {
       multiplyNumbersContentTypeConfig.processors?.[
         ContentTypeMultiplyNumbers.toString()
       ],
-    ).toEqual([processMultiplyNumbers]);
+    ).toEqual([]);
     expect(
       multiplyNumbersContentTypeConfig.validators?.[
         ContentTypeMultiplyNumbers.toString()
       ],
-    ).toEqual(isValidMultiplyNumbersContent);
+    ).toEqual([]);
   });
 
   it("should encode and decode numbers correctly", () => {
@@ -105,10 +89,5 @@ describe("ContentTypeMultiplyNumberCodec", () => {
     const encoded = codec.encode(numbers);
     const decoded = codec.decode(encoded);
     expect(decoded).toEqual(6);
-  }); /*
-  it("should handle known content types", async () => {
-    const testWallet = Wallet.createRandom();
-    const testClient = await Client.create(testWallet, { env: "local" });
-  }); */
-  
+  });
 });
