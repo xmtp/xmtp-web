55a9f3a8657ac71519b11a5c20f94e38be8aeaca
Ry Racherbaumer <ry@xmtp.com>
2024-01-02 13:46:39 -0600

    jsdom => happy-dom for testing

--- packages/react-sdk/vitest.setup.ts
+++ packages/react-sdk/vitest.setup.ts
@@ -1,7 +1,2 @@
 import "fake-indexeddb/auto";
 import "@testing-library/jest-dom/vitest";
-import { Buffer } from "buffer";
-import { webcrypto } from "crypto";
-
-globalThis.Buffer = Buffer;
-globalThis.crypto = webcrypto as unknown as Crypto;
