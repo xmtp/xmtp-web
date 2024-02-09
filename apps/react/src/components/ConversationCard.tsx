d136f69fb597777ce9b93006d8ef2f39b342ff7b
Ry Racherbaumer <ry@xmtp.com>
2024-02-08 19:40:07 -0600

    Update `ConversationCard` component

--- apps/react/src/components/ConversationCard.tsx
+++ apps/react/src/components/ConversationCard.tsx
@@ -1,5 +1,6 @@
-import type { CachedConversation } from "@xmtp/react-sdk";
+import type { CachedConversation, ConsentState } from "@xmtp/react-sdk";
 import { useLastMessage, useConsent } from "@xmtp/react-sdk";
+import { useEffect, useState } from "react";
 import { ConversationPreview } from "../controllers/ConversationPreview";
 
 type ConversationCardProps = {
@@ -13,8 +14,17 @@ export const ConversationCard: React.FC<ConversationCardProps> = ({
   onConversationClick,
   isSelected,
 }) => {
+  const [consentState, setConsentState] = useState<ConsentState>("unknown");
   const lastMessage = useLastMessage(conversation.topic);
-  const { consentState } = useConsent();
+  const { consentState: _consentState } = useConsent();
+
+  useEffect(() => {
+    const getState = async () => {
+      setConsentState(await _consentState(conversation.peerAddress));
+    };
+    void getState();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ConversationPreview
@@ -23,7 +33,7 @@ export const ConversationCard: React.FC<ConversationCardProps> = ({
       isSelected={isSelected}
       onClick={onConversationClick}
       lastMessage={lastMessage}
-      consentState={consentState(conversation.peerAddress)}
+      consentState={consentState}
     />
   );
 };
