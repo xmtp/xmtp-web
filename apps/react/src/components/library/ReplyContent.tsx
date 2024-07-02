289f77cd996eb7a3ec8a22dd29ed3988d7cf1c55
Ry Racherbaumer <ry@xmtp.com>
2024-07-02 15:07:02 -0500

    Refactor React app to work with latest JS SDK

--- apps/react/src/components/library/ReplyContent.tsx
+++ apps/react/src/components/library/ReplyContent.tsx
@@ -1,6 +1,7 @@
 import type { CachedMessage } from "@xmtp/react-sdk";
-import { ContentTypeId, useReply } from "@xmtp/react-sdk";
+import { useReply } from "@xmtp/react-sdk";
 import type { Reply } from "@xmtp/content-type-reply";
+import { ContentTypeId } from "@xmtp/content-type-primitives";
 import { MessageContent } from "./MessageContent";
 
 type ReplyProps = {
